5d3c33f6bf5e171d0b6da957d7ee9179cbc432eb
Hamad <hamadullah16@gmail.com>
2024-12-01 05:14:16 +0500

    db unique key error fixed

--- models/Job.js
+++ models/Job.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const JobSchema = new mongoose.Schema({
     poster: {
         type: String,
-        unique: true,
         required: true
     },
     title: {
